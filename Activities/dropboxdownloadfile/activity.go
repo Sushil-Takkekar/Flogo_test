6cf4da003c23c7bf87ddd04ca00c244f8e5b8045
Sushilkumar Takkekar <stakkeka@tibco.com>
2018-05-12 01:12:40 +0530

    Separated logic for dropbox download file

--- Activities/dropboxdownloadfile/activity.go
+++ Activities/dropboxdownloadfile/activity.go
@@ -1,20 +1,15 @@
 package dropboxdownloadfile
 
 import (
-	"encoding/json"
-	"errors"
-	"io/ioutil"
-	"net/http"
-	"strings"
-
+	"github.com/Sushil-Takkekar/Flogo_test/Activity_logic/dropboxcode"
 	"github.com/TIBCOSoftware/flogo-lib/core/activity"
 	"github.com/TIBCOSoftware/flogo-lib/logger"
 )
 
-// Downloaderror is the struct for download error
-type Downloaderror struct {
-	ErrorSummary string
-}
+// // Downloaderror is the struct for download error
+// type Downloaderror struct {
+// 	ErrorSummary string
+// }
 
 // ActivityLog is the default logger for the Log Activity
 var activityLog = logger.GetLogger("activity-flogo-Dropbox_DownloadFile")
@@ -43,38 +38,14 @@ func (a *DropboxDownloadFileActivity) Eval(context activity.Context) (done bool,
 	accessToken := context.GetInput("accessToken").(string)
 	DropboxAPIArg := `{"path": "` + context.GetInput("downloadSourcePath").(string) + `"}`
 
-	request, _ := http.NewRequest("POST", "https://content.dropboxapi.com/2/files/download", nil)
-	request.Header.Set("Authorization", "Bearer "+accessToken)
-	request.Header.Set("Dropbox-API-Arg", DropboxAPIArg)
-	client := &http.Client{}
-	resDownloadFile, errDownloadFile := client.Do(request)
-	if errDownloadFile != nil {
-		//fmt.Printf(errDownloadFile.Error())
-		activityLog.Errorf(errDownloadFile.Error())
-		return false, errDownloadFile
-	}
-	resDownloadFileData, _ := ioutil.ReadAll(resDownloadFile.Body)
-	if strings.Contains(string(resDownloadFileData), "Error in call to API function") {
-		//fmt.Println("Error= ", string(resDownloadFileData))
-		activityLog.Errorf(string(resDownloadFileData))
-		return false, errors.New(string(resDownloadFileData))
-	}
-	if strings.Contains(string(resDownloadFileData), "Unknown API function") {
-		//fmt.Println("Error= ", string(resDownloadFileData))
-		activityLog.Errorf(string(resDownloadFileData))
-		return false, errors.New(string(resDownloadFileData))
-	}
+	result, err := dropboxcode.downloadFile(accessToken, DropboxAPIArg)
 
-	var downloaderror Downloaderror
-	json.Unmarshal([]byte(string(resDownloadFileData)), &downloaderror)
-	if downloaderror.ErrorSummary != "" {
-		//fmt.Println("error_summary=", downloaderror.ErrorSummary)
-		activityLog.Errorf(downloaderror.ErrorSummary)
-		return false, errors.New(downloaderror.ErrorSummary)
+	if err != nil {
+		activityLog.Errorf(err.Error())
+		return false, err
 	}
-
 	//fmt.Println("resDownloadFileData= ", string(resDownloadFileData))
-	activityLog.Debugf("resDownloadFileData: %s", resDownloadFileData)
-	context.SetOutput("fileContents", resDownloadFileData)
+	activityLog.Debugf("Result: %s", result)
+	context.SetOutput("fileContents", result)
 	return true, nil
 }
---
+++ Activity_logic/dropboxcode/downloadFile.go
@@ -0,0 +1,53 @@
+package dropboxcode
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+// Downloaderror is the struct for download error
+type Downloaderror struct {
+	ErrorSummary string
+}
+
+// SetSurveyDetails maps the required data from survey response to output
+func downloadFile(accessToken string, DropboxAPIArg string) (result []byte, err error) {
+
+	var res []byte
+	request, _ := http.NewRequest("POST", "https://content.dropboxapi.com/2/files/download", nil)
+	request.Header.Set("Authorization", "Bearer "+accessToken)
+	request.Header.Set("Dropbox-API-Arg", DropboxAPIArg)
+	client := &http.Client{}
+	resDownloadFile, errDownloadFile := client.Do(request)
+	if errDownloadFile != nil {
+		//fmt.Printf(errDownloadFile.Error())
+		//activityLog.Errorf(errDownloadFile.Error())
+		return res, errDownloadFile
+	}
+	resDownloadFileData, _ := ioutil.ReadAll(resDownloadFile.Body)
+	if strings.Contains(string(resDownloadFileData), "Error in call to API function") {
+		//fmt.Println("Error= ", string(resDownloadFileData))
+		//activityLog.Errorf(string(resDownloadFileData))
+		return res, errors.New(string(resDownloadFileData))
+	}
+	if strings.Contains(string(resDownloadFileData), "Unknown API function") {
+		//fmt.Println("Error= ", string(resDownloadFileData))
+		//activityLog.Errorf(string(resDownloadFileData))
+		return res, errors.New(string(resDownloadFileData))
+	}
+
+	var downloaderror Downloaderror
+	json.Unmarshal([]byte(string(resDownloadFileData)), &downloaderror)
+	if downloaderror.ErrorSummary != "" {
+		//fmt.Println("error_summary=", downloaderror.ErrorSummary)
+		//activityLog.Errorf(downloaderror.ErrorSummary)
+		return res, errors.New(downloaderror.ErrorSummary)
+	}
+	//fmt.Println("resDownloadFileData= ", string(resDownloadFileData))
+	//activityLog.Debugf("resDownloadFileData: %s", resDownloadFileData)
+	//context.SetOutput("fileContents", resDownloadFileData)
+	return resDownloadFileData, nil
+}
